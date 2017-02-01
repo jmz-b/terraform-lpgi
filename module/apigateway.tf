43567f28d1d36a8937b7536c146ee43eb86354fc
jmz <jmz-b@users.noreply.github.com>
2017-02-01 12:55:01 +0000

    manage stage name spacing at file level

--- module/apigateway.tf
+++ module/apigateway.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_rest_api" "proxy" {
-	name = "${var.name}-${var.stage}-proxy"
+	name = "${var.name}-proxy"
 }
 
 # root resource
--- module/s3.tf
+++ module/s3.tf
@@ -1,15 +1,14 @@
 resource "aws_s3_bucket" "dist_bucket" {
-	bucket = "${var.name}-${var.stage}-dist"
+	bucket = "${var.name}-dist"
 	acl = "private"
 	tags {
 		Name = "${var.name}"
-		Environment = "${var.stage}"
 	}
 }
 
 resource "aws_s3_bucket_object" "default_dist" {
-	key = "${var.name}-dist.zip"
-	bucket = "${var.name}-${var.stage}-dist"
+	key = "${var.name}-${var.stage}-dist.zip"
+	bucket = "${var.name}-dist"
 	source = "${path.module}/dist.zip"
 	etag = "${md5(file("${path.module}/dist.zip"))}"
 	depends_on = ["aws_s3_bucket.dist_bucket"]
